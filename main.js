1e1fdc91e827aa795bfd664fc9d73f983a614787
robert-virtual <robertocastill63@gmail.com>
2022-02-19 15:12:20 -0600

    little change⚡

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ let localStream = new MediaStream();
     call.on("stream", function (remoteStream) {
       // `stream` is the MediaStream of the remote peer.
       // Here you'd add it to an HTML video/canvas element.
-      addVideoStream(remoteStream, true);
+      // addVideoStream(remoteStream, true);
     });
   });
 })();
