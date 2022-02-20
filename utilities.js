463aac97e227cb05a35eece083ce386b033178e4
robert-virtual <robertocastill63@gmail.com>
2022-02-20 11:31:03 -0600

    no duplicate element ⛏

--- utilities.js
+++ utilities.js
@@ -1,13 +1,15 @@
 const videoGrid = document.getElementById("video-grid");
 
 export function addVideoStream(video, stream, msg, muted = false) {
-  let div = document.createElement("div");
-  let p = document.createElement("p");
-  p.textContent = msg;
-  video.srcObject = stream;
-  video.muted = muted;
-  video.addEventListener("loadedmetadata", () => video.play());
-  div.append(video);
-  div.append(p);
-  videoGrid.append(div);
+  if (!document.contains(video)) {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    p.textContent = msg;
+    video.srcObject = stream;
+    video.muted = muted;
+    video.addEventListener("loadedmetadata", () => video.play());
+    div.append(video);
+    div.append(p);
+    videoGrid.append(div);
+  }
 }
